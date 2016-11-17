d45b60755bdc672f6df8ba37339bbcfa7bbab40c
Filip Dalüge <daluege@gmail.com>
2016-11-17 14:08:02 +0100

    Add Promise support

--- operate.ts
+++ operate.ts
@@ -1,8 +1,4 @@
-exports = operate
-
-export declare var zone: Zone
-
-zone = null
+module.exports = exports = operate
 
 const Timers: { set: string, clear: string, once: boolean }[] = [
   { set: 'setImmediate', clear: 'clearImmediate', once: true },
@@ -15,7 +11,7 @@ export interface ZoneOptions {
   callback?: (callback: Function) => any
 }
 
-export default function operate (callback: Function, options?: Zone, context: any = {}): PromiseLike<any> {
+export default function operate (callback: () => void, context: any = {}, options?: Zone): PromiseLike<any> {
   let zone = new Zone(callback.name, options, context)
   setImmediate(() => zone.run(callback))
   return zone
@@ -27,28 +23,30 @@ export interface Task {
   cancel: () => void
 }
 
-export class Zone extends Promise<any> {
+export class Zone implements PromiseLike<any> {
   static current: Zone = null
 
+  parent: Zone = null
   value = undefined
   running = false
-  blocking = true
   tasks = new Set<Task>()
 
   private types = new Map<any, Map<any, Task>>()
+  private promise = new Promise<any>((resolve, reject) => (this.resolve = resolve, this.reject = reject))
   private resolve: (global: any) => void
   private reject: (error: any) => void
 
-  constructor (public id?: string, options?: ZoneOptions, public context: any = {}) {
-    super((resolve, reject) => {
-      this.resolve = resolve
-      this.reject = reject
-    })
-
+  constructor (public name?: string, public context: any = {}, private options: ZoneOptions = {}) {
     if (options != null) Object.assign(this, options)
   }
 
+  static operate = operate
+
   callback = (callback: Function) => callback()
+
+  fork (name?: string, options?: any) {
+    return new Zone(name == null ? this.name : name, Object.create(this.context))
+  }
 
   add (type: any, id: any, cancel: () => void): Task {
     let task: Task = { type, id, cancel }
@@ -56,7 +54,7 @@ export class Zone extends Promise<any> {
     if (id == null) return task
 
     let tasks = this.types.get(type)
-    if (!tasks) this.types.set(type, (tasks = new Map()))
+    if (!tasks) this.types.set(type, (tasks = new Map<any, Task>()))
     tasks.set(id, task)
     return task
   }
@@ -79,39 +77,41 @@ export class Zone extends Promise<any> {
     return this.tasks.delete(task)
   }
 
-  cancel (): void {
+  then (resolve: (value: any) => any, reject: (reason: any) => any): Promise<any> {
+    return this.promise.then(resolve, reject)
+  }
+
+  cancel (reason?: any): void {
     for (let task of this.tasks.values()) {
       try { task.cancel() }
       catch (error) { this.reject(error) }
     }
     this.tasks.clear()
     this.running = false
+    if (reason != null) this.reject(reason)
+    else this.resolve(this.value)
   }
 
-  run (callback: Function, thisArg: any = null, ...args: any[]): any {
-    if (arguments.length > 1) return this.run(() => callback.apply(thisArg, args))
-
-    let globalZone = zone
-    let currentZone = Zone.current
+  do (callback: () => any) {
+    this.parent = Zone.current
     try {
-      zone = Zone.current = this
+      Zone.current = this
       this.running = true
 
       return this.callback(callback)
     } catch (error) {
-      this.cancel()
-      this.reject(error)
+      this.cancel(error)
     } finally {
-      zone = globalZone
-      Zone.current = currentZone
-
-      if (args.length) this.value = args[0]
-
-      if (!this.tasks.size) {
-        this.cancel()
-        this.resolve(this.value)
-      }
+      Zone.current = this.parent
+      if (!this.tasks.size) this.cancel()
     }
+  }
+
+  run (callback: () => any, thisArg?: any, ...args: any[]): any {
+    if (arguments.length > 1) return this.run(() => callback.apply(thisArg, args))
+
+    this.value = this.do(callback)
+    return this.value
   }
 }
 
@@ -120,13 +120,14 @@ for (let timer of Timers) {
   let clear = global[timer.clear]
   let type = set
 
-  global[timer.set] = (callback: Function, ...args: any[]) => {
+  global[timer.set] = (callback: () => void, ...args: any[]) => {
+    let zone = Zone.current
     if (!zone) return set(callback, ...args)
 
     let id = set(
       () => {
         if (timer.once) zone.delete(set, id)
-        callback()
+        zone.do(callback)
       },
       ...args)
 
@@ -134,6 +135,7 @@ for (let timer of Timers) {
     return id
   }
   global[timer.clear] = (id: any) => {
+    let zone = Zone.current
     if (!zone) return clear(id)
 
     // Verify that the task is owned by the current zone
@@ -143,4 +145,23 @@ for (let timer of Timers) {
 
     zone.delete(type, id)
   }
+}
+
+const then = Promise.prototype.then
+
+Promise.prototype.then = function (resolve, reject): any {
+  let cancelled = false
+
+  const zone = Zone.current
+  if (zone == null) return then.apply(this, arguments)
+
+  const execute = callback => {
+    if (typeof callback === 'function') {
+      return value => !cancelled && zone.tasks.delete(task) && zone.do(() => callback(value))
+    }
+  }
+  const result = then.call(this, execute(resolve), execute(reject))
+  const task = zone.add(Promise, null, () => (cancelled = true))
+
+  return result
 }
